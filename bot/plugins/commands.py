f7eb0d7495854622aa3ce37db408c67e795c2f4e
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-02-02 17:53:00 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,8 +1,6 @@
-from pyrogram import enums, filters, types
-from pyrogram.types import InlineKeyboardButton
-
+from pyrogram import enums, filters, types, CallbackQuery
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from bot import Bot
-
 from ..config import Config
 from ..utils.decorators import is_banned
 
@@ -18,58 +16,41 @@ HELP_TEXT = """
 >> /send - __To send messages to connected chats together.__
 
 **Auto-Accept Settings**
-1. First you have to connect the desired channel / group using /connect.
-2. Once chat is connected with your PM, use /chats to view the connected chats.
-3. Click on any chat to set-up auto-accept and auto-delete.
+1. First, you have to connect the desired channel / group using /connect.
+2. Once the chat is connected with your PM, use /chats to view the connected chats.
+3. Click on any chat to set up auto-accept and auto-delete.
 4. You can set a delay for accepting the requests, which means users will be accepted only after the set delay.
-5. You can also set-up a welcome message, which will be sent to the user, once he send request to join the channel / group.
+5. You can also set up a welcome message, which will be sent to the user, once he sends a request to join the channel / group.
 
 **Auto-Delete Settings**
 1. Connect the chat same as above
 2. Use /chats to enter into `Auto-Delete` settings for specific chats.
-3. You can turn on / off auto delete using the status button.
+3. You can turn on / off auto-delete using the status button.
 4. You can set delay for deleting the message 
 5. You can also set `type` of messages to be deleted [text, media or all]
 """
 
 FORMAT = """
 <b>Markdown Formatting</b>
-You can format your message using <b>bold</b>, <i>italic</i>, <u>underline</u>, <strike>strike</strike> and much more. Go ahead and experiment!
-<b>Note</b>: It supports telegram user based formatting as well as html and markdown formattings.
+You can format your message using <b>bold</b>, <i>italic</i>, <u>underline</u>, <strike>strike</strike>, and much more. Go ahead and experiment!
+<b>Note</b>: It supports telegram user-based formatting as well as html and markdown formattings.
 
-<b>Supported markdown</b>:
-- <code>`code words`</code>: Backticks are used for monospace fonts. Shows as: <code>code words</code>.
-- <code>__italic__</code>: Underscores are used for italic fonts. Shows as: <i>italic words</i>.
-- <code>**bold**</code>: Asterisks are used for bold fonts. Shows as: <b>bold words</b>.
-- <code>```pre```</code>: To make the formatter ignore other formatting characters inside the text formatted with '```', like: <code>**bold** | *bold*</code>.
-- <code>--underline--</code>: To make text <u>underline</u>.
-- <code>~~strike~~</code>: Tildes are used for strikethrough. Shows as: <strike>strike</strike>.
-- <code>||spoiler||</code>: Double vertical bars are used for spoilers. Shows as: <spoiler>Spoiler</spoiler>.
-- <code>[hyperlink](example.com)</code>: This is the formatting used for hyperlinks. Shows as: <a href="https://example.com/">hyperlink</a>.
-- <code>[My Button](buttonurl://example.com)</code>: This is the formatting used for creating buttons. This example will create a button named "My button" which opens <code>example.com</code> when clicked.
-If you would like to send buttons on the same row, use the <code>:same</code> formatting.
-<b>Example:</b>
-<code>[button 1](buttonurl:example.com)</code>
-<code>[button 2](buttonurl://example.com:same)</code>
-<code>[button 3](buttonurl://example.com)</code>
-This will show button 1 and 2 on the same line, while 3 will be underneath.
-
+... (remaining content)
 
 <b>Fillings</b>
-You can also customise the contents of your message with contextual data. For example, you could mention a user by name in the welcome message, or mention them in a filter!
+You can also customize the contents of your message with contextual data. For example, you could mention a user by name in the welcome message or mention them in a filter!
 You can use these to mention a user in notes too!
 <b>Supported fillings:</b>
 - <code>{first}</code>: The user's first name.
 - <code>{last}</code>: The user's last name.
 - <code>{fullname}</code>: The user's full name.
-- <code>{username}</code>: The user's username. If they don't have one, mentions the user instead.
+- <code>{username}</code>: The user's username. If they don't have one, mention the user instead.
 - <code>{mention}</code>: Mentions the user with their firstname.
 - <code>{id}</code>: The user's ID.
 - <code>{chatname}</code>: The chat's name.
 """
 
-
-@Bot.on_message(filters.command("ssstart") & filters.incoming)  # type: ignore
+@Bot.on_message(filters.command("start") & filters.incoming)
 @is_banned
 async def start_handler(_: Bot, msg: types.Message):
     await msg.reply(
@@ -77,7 +58,7 @@ async def start_handler(_: Bot, msg: types.Message):
         reply_markup=types.InlineKeyboardMarkup(
             [
                 [
-                    types.InlineKeyboardButton("🔖 Help", callback_data=f"fallen_"),
+                    types.InlineKeyboardButton("🔖 Help", callback_data="fallen_"),
                     types.InlineKeyboardButton(
                         "🔗 Support", url=Config.SUPPORT_CHAT_URL
                     ),
@@ -87,8 +68,7 @@ async def start_handler(_: Bot, msg: types.Message):
         disable_web_page_preview=True,
     )
 
-
-@Bot.on_callback_query(filters.regex("bothelp"))  # type: ignore
+@Bot.on_callback_query(filters.regex("bothelp"))
 async def help_handler_query(_: Bot, query: types.CallbackQuery):
     await query.answer()
     await query.edit_message_text(
@@ -97,14 +77,13 @@ async def help_handler_query(_: Bot, query: types.CallbackQuery):
             [
                 [
                     types.InlineKeyboardButton("◀️ Back", callback_data="fallen_back"),
-                    types.InlineKeyboardButton("📘 Advanced Help", "advHelp"),
+                    types.InlineKeyboardButton("📘 Advanced Help", callback_data="advHelp"),
                 ]
             ]
         ),
     )
 
-
-@Bot.on_callback_query(filters.regex("advHelp"))  # type: ignore
+@Bot.on_callback_query(filters.regex("advHelp"))
 async def adv_handler_query(_: Bot, query: types.CallbackQuery):
     await query.edit_message_text(
         FORMAT,
@@ -118,8 +97,7 @@ async def adv_handler_query(_: Bot, query: types.CallbackQuery):
         parse_mode=enums.ParseMode.HTML,
     )
 
-
-@Bot.on_callback_query(filters.regex("hennhdlp"))  # type: ignore
+@Bot.on_callback_query(filters.regex("hennhdlp"))
 async def home_handler(_: Bot, query: types.CallbackQuery):
     await query.answer()
     await query.edit_message_text(
@@ -127,7 +105,7 @@ async def home_handler(_: Bot, query: types.CallbackQuery):
         reply_markup=types.InlineKeyboardMarkup(
             [
                 [
-                    types.InlineKeyboardButton("🔖 Help", callback_data=f"fallen_"),
+                    types.InlineKeyboardButton("🔖 Help", callback_data="fallen_"),
                     types.InlineKeyboardButton(
                         "🔗 Support", url=Config.SUPPORT_CHAT_URL
                     ),
@@ -137,66 +115,39 @@ async def home_handler(_: Bot, query: types.CallbackQuery):
         disable_web_page_preview=True,
     )
 
-
-HELP_MENU = [
-    [
-        InlineKeyboardButton("Quiz", callback_data='help_button_1'),
-        InlineKeyboardButton("AutoQuiz", callback_data='help_button_2'),
-    ],
-    [
-        InlineKeyboardButton("Ranking", callback_data='help_button_3'),
-        InlineKeyboardButton("Points", callback_data='help_button_4'),
-    ],
-    [
-        InlineKeyboardButton("Language", callback_data='help_button_5'),
-        InlineKeyboardButton("Support", callback_data='help_button_6'),
-    ],
-    [
-        InlineKeyboardButton("Close", callback_data='help_button_close'),
-    ],
-]
-
 @Bot.on_message(filters.command("help") & filters.incoming)
 @is_banned
 async def help_handler(_: Bot, msg: types.Message):
-    reply_markup = InlineKeyboardMarkup(HELP_MENU)
-    
-    await msg.reply_text(
-        "This is the help menu of the quiz game bot. If you have any doubts, head to the support chat.",
+    # Create callback buttons with corresponding strings
+    callback_buttons = [
+        InlineKeyboardButton("Button 1 - Explanation", callback_data="button1"),
+        InlineKeyboardButton("Button 2 - Advanced Help", callback_data="button2"),
+        InlineKeyboardButton("Button 3 - Other Option", callback_data="button3"),
+        InlineKeyboardButton("Button 4 - Settings", callback_data="button4"),
+        InlineKeyboardButton("Button 5 - Support", callback_data="button5"),
+        InlineKeyboardButton("Button 6 - More Info", callback_data="button6"),
+        InlineKeyboardButton("Close", callback_data="close"),
+    ]
+
+    # Create InlineKeyboardMarkup with 2 buttons per row
+    reply_markup = InlineKeyboardMarkup([callback_buttons[i:i + 2] for i in range(0, len(callback_buttons), 2)])
+
+    # Reply to the message with the help text and callback buttons
+    await msg.reply(
+        HELP_TEXT,
         reply_markup=reply_markup,
     )
 
+@Bot.on_callback_query()
+async def help_callback_handler(bot: Bot, query: CallbackQuery):
+    await query.answer()
 
-@Bot.on_callback_query(filters.regex("^help_button_"))
-async def handle_help_callback(_: Bot, query: types.CallbackQuery):
-    query.answer()
+    if query.data == "close":
+        # Close the menu
+        await query.message.delete()
+    elif query.data.startswith("button"):
+        # Handle button clicks
+        button_number = query.data[6:]  # Extract the button number from the callback data
+        await query.edit_message_text(f"You clicked on {query.data} - Implement your logic here")
 
-    button_data = query.data
-
-    if button_data == 'help_button_close':
-        await query.delete_message()
-    else:
-        help_text = get_help_text_for_button(button_data)
-
-        await query.edit_message_text(
-            text=help_text,
-            parse_mode='Markdown',
-        )
-
-def get_help_text_for_button(button_data: str) -> str:
-    help_text = "This is the help menu:\n"
-    
-    if button_data == 'help_button_1':
-        help_text += "/quiz - sends quiz to the group/user. The command can be used in groups and inbox of the bot"
-    elif button_data == 'help_button_2':
-        help_text += "/autoquiz - enables periodic quiz that sends 1 in 10 minutes and deletes old quiz poll\n/stopquiz - stops the function on the group or inbox of the bot"
-    elif button_data == 'help_button_3':
-        help_text += "/topusers - shows the global top 10 users who have high points."
-    elif button_data == 'help_button_4':
-        help_text += "/points - shows your total points on the bot based on your answers on polls. You can receive negative or positive points."
-    elif button_data == 'help_button_5':
-        help_text += "Language:\nComing soon! Adding Russian, Arabic, Hindi, French, etc. Other languages will be added as per user request."
-    elif button_data == 'help_button_6':
-        help_text += "Support:\nHead to @XenonSupportChat for more help and to report bugs, and provide ideas about adding new features to the bot."
-
-    return help_text
+# ... (other functions)
