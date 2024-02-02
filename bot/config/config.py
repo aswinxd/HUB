8e3218aa08efa6c64a394e010020e2cdbd6cb197
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-02-02 16:43:26 +0530

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -8,8 +8,8 @@ class Config:
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "6350652136:AAFriDrVaXsIEchvLTj8BY3JEvvCGyVjTHI")
   BOT_NAME = os.environ.get("BOT_NAME", "erewew")
 
-  API_ID = int(os.environ.get("API_ID", "15851949"))
-  API_HASH = os.environ.get("API_HASH", "f9e386978bf103c4a6baab0a4b92e822")
+  API_ID = int(os.environ.get("API_ID", "12799559"))
+  API_HASH = os.environ.get("API_HASH", "077254e69d93d08357f25bb5f4504580")
 
   DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://bot:bot@cluster0.8vepzds.mongodb.net/?retryWrites=true&w=majority")
   SESSION_NAME = os.environ.get("DATABASE_NAME", "Aleena")
